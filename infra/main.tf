42d6a40b7c4f67b9f10645637f3a25c64cd77d38
adeoluadeyemi <adeolu.ooa@gmail.com>
2024-08-13 16:33:39 +0100

    Removed state management config from TF file

--- infra/main.tf
+++ infra/main.tf
@@ -1,3 +1,13 @@
+# terraform {
+#   backend "s3" {
+#       bucket = "terraform-state-backend"
+#       key = "terraform.tfstate"
+#       region = "eu-west-2"
+#       dynamodb_table = "terraform_state"
+#   }
+# }
+
+
 provider "aws" {
   region = "eu-west-2"
 }
