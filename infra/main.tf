c3b114fd8ecf17f2c6ac54faffc9aac2c932bcf9
adeoluadeyemi <adeolu.ooa@gmail.com>
2024-08-14 01:58:45 +0100

    Removed blank line

--- infra/main.tf
+++ infra/main.tf
@@ -1,12 +1,10 @@
 terraform {
-
   backend "s3" {
     bucket = "govuk-fe-demo-terraform-state-backend"
     key = "terraform.tfstate"
     region = "eu-west-2"
     dynamodb_table = "terraform_state"
   }
-
 }
 
 
