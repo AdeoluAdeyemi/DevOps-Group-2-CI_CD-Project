c7c9c4e8c06e3f75ccf753dd0a1e624846c28576
Adeolu Adeyemi <98715389+AdeoluAdeyemi@users.noreply.github.com>
2024-08-16 13:39:46 +0100

    Improve workflow (#17)
    * Removed duplicate TF codes from workflow
    
    * Updated the variable files
    
    * Update container name and port
    
    * Added policy for Dynamodb
    
    * renamed workflows
    
    * renamed workflows

--- infra/s3-dynamodb-state/variables.tf
+++ infra/s3-dynamodb-state/variables.tf
@@ -1,19 +1,19 @@
 variable "s3_bucket_name" {
   description = "Name of S3 bucket"
-  type = string
+  type        = string
 }
 
 variable "dynamodb_table_name" {
   description = "Name of DynamoDB table"
-  type = string
+  type        = string
 }
 
-variable "dynamodb_hash" { 
+variable "dynamodb_hash" {
   description = "DynamoDB table hash key"
-  type = string
+  type        = string
 }
 
 variable "s3_bucket_versioning" {
   description = "Status of s3 bucket versioning"
-  type = string
+  type        = string
 }
